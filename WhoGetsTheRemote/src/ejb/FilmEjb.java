5ba65ae7d28f45179cb09524ba8cd45118d6dd5f
mikemouannes <mike_mouannes@hotmail.com>
2016-01-21 13:01:53 +0100

    commiting from cmd for film EJB

--- WhoGetsTheRemote/src/ejb/FilmEjb.java
+++ WhoGetsTheRemote/src/ejb/FilmEjb.java
@@ -1,5 +1,6 @@
 package ejb;
 
+import java.io.Console;
 import java.util.List;
 
 import javax.ejb.Asynchronous;
@@ -16,42 +17,46 @@ import models.Film;
 @Asynchronous
 public class FilmEjb implements FilmEjbLocal {
 
+	
+	List <Film> myList = null;
     /**
      * Default constructor. 
      */
     public FilmEjb() {
         // TODO Auto-generated constructor stub
+    	System.out.println("i am in film ejb constructor");
+    	for (int i=0; i<4; i++)
+    	{
+    		Film f= new Film (i, "Film"+i, "this is the Description" + i , "Director name"+i , "16/02/2001", 3);
+    		myList.add(f);    		
+    	}
     }
     
     @Override
     public void addFilm(Film film)
     {
-    	
+    	myList.add(film);
     }
     
     @Override
     public void editFilm(Film film)
     {
-    	
+    	//myList.
     }
     @Override
     public void deleteFilm(int filmId)
     {
-    	
+    	myList.remove(filmId);
     }
     @Override
     public Film getFilm(int filmId)
     {
-		return null;    	
+    	myList.get(filmId);
+		return null;		
     }
     @Override
     public List<Film> getAllFilm()
-    {
-    	List <Film> myList;
-    	for (int i=0; i<3; i++)
-    	{
-    		
-    	}
-		return null;    	
+    {    	    
+		return myList;    	
     }
 }
