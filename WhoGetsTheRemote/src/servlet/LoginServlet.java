46dbbdc9a62edb12d2355a506adad64159908a34
dchouba <daniel.choubassi@gmail.com>
2016-06-05 23:37:16 +0200

    Login servlet et login page

--- WhoGetsTheRemote/src/servlet/LoginServlet.java
+++ WhoGetsTheRemote/src/servlet/LoginServlet.java
@@ -123,7 +123,6 @@ public class LoginServlet extends HttpServlet {
 
 				User currentUser = new User(id,prenom,nom,usernamee,passwordd,emaill,Bday, sexee, addresss, imagee, modificationDatee, creationDatee,friendsInCommon);
 				                            
-				request.setAttribute("currentUser", currentUser);	
 				
 				//Liste des nouveaux films
 				PreparedStatement statementNewFilms = myConnection.prepareStatement("SELECT * FROM FILM ORDER BY FILM_CREATION_DATE FETCH FIRST 8 ROWS ONLY");
@@ -139,7 +138,9 @@ public class LoginServlet extends HttpServlet {
 					String trailer = newMoviesResult.getString("TRAILER_FILM_LINK");
 					String filmLink =  newMoviesResult.getString("FILM_LINK");
 					String image =  newMoviesResult.getString("IMAGE_FILM");
-					Film currentFilm = new Film(idFilm,nomFilm,descriptionFilm,dateReleased,notationFilm,trailer,filmLink,image) ;
+					String creationDate =  newMoviesResult.getString("FILM_CREATION_DATE");
+					String modificationDate =  newMoviesResult.getString("FILM_MODIFICATION_DATE");
+					Film currentFilm = new Film(idFilm,nomFilm,descriptionFilm,dateReleased,notationFilm,trailer,filmLink,image,creationDate,modificationDate) ;
 					listF.add(currentFilm);
 					
 					int i = 0;
@@ -154,7 +155,7 @@ public class LoginServlet extends HttpServlet {
 				session = request.getSession();
 				session.setAttribute("userLogged", currentUser);
 				session.setAttribute("listHits", listF);
-						
+				session.setAttribute("sessionId", currentUser.getId());			
 				request.getRequestDispatcher("/Home.jsp").forward(request, response);
 			} 
             else 
